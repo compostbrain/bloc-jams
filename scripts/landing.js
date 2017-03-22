bdf44407530c96635c3a2c40e2f5d4e3d58972ae
Joel Cahalan <joel@wildmeadowsfarm.com>
2017-03-21 21:53:19 -0400

    assignemnt-9: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,15 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function (points) {
-    
-    var revealPoint = function(index){           
-        points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0)";
-        points[index].style.msTransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+var revealPoint = function(point){           
+        point.style.opacity = 1;
+        point.style.transform = "scaleX(1) translateY(0)";
+        point.style.msTransform = "scaleX(1) translateY(0)";
+        point.style.WebkitTransform = "scaleX(1) translateY(0)";
         };
-    
-    for (var i=0; i<points.length; i++){
-        revealPoint(i);
-    }
+
+var animatePoints = function (points) {
+    forEach(points, revealPoint);
+   
  };
 window.onload = function() {
     if (window.innerHeight > 950) {
@@ -21,7 +19,7 @@ window.onload = function() {
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
     
     window.addEventListener('scroll', function(event) {
-      console.log("Current offset from the top is" + sellingPoints.getBoundingClientRect().top + " pixels"); 
+      
         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
             animatePoints(pointsArray);
         }
